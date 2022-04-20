446a2835d97ebb485e1b0b88711fa54bf7cca57c
Pieter Vanslambrouck <pieter.vanslambrouck@student.kuleuven.be>
2022-04-20 20:22:44 +0200

    add initial files

---
+++ seesaw.m
@@ -0,0 +1,17 @@
+Rm = 2.6;
+Kt = 0.00767;
+Km = 0.00767;
+Kg = 3.71;
+eta_m = 1;
+eta_g = 1;
+Mc = 0.52;
+Msw = 3.6;
+% Jsw is the moment of inertia about the center of gravity, should be about
+% pivot point to match theory! I guess there isn't a big difference?
+Jsw = 0.3950;
+Dt = 0.1250;
+Dc = 0.058;
+r = 0.00635;
+g = 9.81;
+Beq =  0.9;
+Bsw = 0;
