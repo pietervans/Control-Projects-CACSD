757cc58588d47d768a4fd88c00ae22bb07db3ab9
Pieter Vanslambrouck <pieter.vanslambrouck@student.kuleuven.be>
2022-04-29 11:09:46 +0200

    improve some stuff

--- Seesaw/closed_loop1.m
+++ Seesaw/closed_loop1.m
@@ -4,13 +4,17 @@ main;
 C = eye(4);
 D = zeros(4,1);
 
-Q = diag([100 400 0 0]);
-R = 1;
+R = 1; % We change Q relative to R
+
+% State vector x = [x  theta  dx  dtheta]'
+% Q = diag([100 400 0 0]);
+Q = diag([100 100 0 0]);
+
+% Try to stay within these limits:
+% |u| < 3.5V (can be increased to 5V)
+% |x| < ?  --> what is length of track?
+
+
 K = lqr(A,B,Q,R);
 
-
-% USED IN SIMULINK DIAGRAM
-% function x_desired = get_x_desired(theta_desired)
-%     % From equation (13)
-%     x_desired = (Mc*Dt+Msw*Dc)*tan(theta_desired)/Mc;
-% end
+clp = eig(A-B*K); % Closed loop poles
--- Seesaw/open_loop.m
+++ Seesaw/open_loop.m
@@ -6,4 +6,18 @@ C = I4(1:2,:);
 D = zeros(2,1);
 
 sys = ss(A,B,C,D);
-pzmap(sys)
+
+[P, Z] = pzmap(sys);
+% System is unstable
+
+CO = ctrb(A,B);
+rank_OC = rank(CO);
+% System is controllable
+
+Ob = obsv(A,C);
+rank_Ob = rank(Ob);
+% System is observable
+
+% System is also stabilizable, since unstable mode is controllable
+% System is also detectable, since unstable mode is observable
+% System is also minimal, since it's controllable and observable
