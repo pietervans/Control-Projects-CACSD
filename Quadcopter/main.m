996892630194f38ce28ed32c080b76a609ede1d7
Pieter Vanslambrouck <pieter.vanslambrouck@student.kuleuven.be>
2022-05-06 14:39:24 +0200

    implement basic controllers for quad

--- Quadcopter/main.m
+++ Quadcopter/main.m
@@ -51,6 +51,7 @@ sys = ss(A,B,C,D);
 
 
 % DISCRETIZATION
+% -------------------------------------------------------------------------
 Ts = 0.05;
 G_d = c2d(sys,Ts,'tustin');
 Az = G_d.A; Bz = G_d.B; Cz = G_d.C; Dz = G_d.D;
@@ -69,13 +70,34 @@ rank_Ob = rank(Ob); % System is observable
 
 
 
-% LQR CONTROL
-N = [Az-eye(12), Bz; Cz Dz]\[zeros(12,6); eye(6)]; % Least-squares solution
+% LQR CONTROL, see quadcopter_lqr.slx
+% -------------------------------------------------------------------------
+N = [Az-eye(12) Bz; Cz Dz]\[zeros(12,6); eye(6)]; % Least-squares solution
 Nx = N(1:12,:);
 Nu = N(13:end,:);
 
 R = diag([1 1 1 1]); % Tune Q relative to R, 0<u<100
-Q = 100*diag([1 1 1 0 0 0 10 10 1 0 0 0]);
+Q = 100*diag([1 1 1 0 0 0 30 30 1 0 0 0]);
 
 [K, ~, clp] = dlqr(Az,Bz,Q,R);
 
+
+% LQR CONTROL with integrator, quadcopter_lqr_integrator.slx
+% -------------------------------------------------------------------------
+% State is equal to 15 dimensional vector:
+% [integral(x-x_ref); integral(y-y_ref); integral(z-z_ref); original_state]
+
+Aint = [eye(3) Cz(1:3,:); zeros(12,3) Az];
+Bint = [Dz(1:3,:); Bz];
+Cint = [zeros(6,3) Cz];
+Dint = Dz;
+
+CO = ctrb(Aint, Bint);
+rank_OC = rank(CO); % Augmented system is controllable
+
+Rint = diag([1 1 1 1]); % Tune Q relative to R
+Qint = 1e7*diag([1 1 1, 1 1 1, 0 0 0, 1 1 1, 0 0 0]);
+
+[Kint_all, ~, clp_int] = dlqr(Aint,Bint,Qint,Rint);
+Kint = Kint_all(:,1:3); % Gain for integrated errors
+Ks = Kint_all(:,4:end); % Gain for 12D state vector
